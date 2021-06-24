775b1aae767028395187a8ce91b2b2690b7f794d
Rashaduzamman Rian <rian.acca@gmail.com>
2021-06-24 18:16:45 +0600

    update

--- index.js
+++ index.js
@@ -39,12 +39,12 @@ client.connect((err) => {
   app.get("/dMatched", (req, res) => {
     const for_d = "d";
     leadsCollection.find({ for_d: for_d }).toArray((err, d) => {
-      console.log(d);
       res.send(d);
     });
   });
   app.patch("/answers/:id", (req, res) => {
     const answers = req.body;
+    console.log(answers);
     const id = ObjectID(req.params.id);
     leadsCollection
       .updateOne(
@@ -63,6 +63,7 @@ client.connect((err) => {
             answer6: answers.ans6,
             answer7: answers.ans7,
             answer8: answers.ans8,
+            agentID:answers.agentID
           },
         }
       )
@@ -92,33 +93,68 @@ client.connect((err) => {
         res.send(update);
       });
   });
+  app.patch("/updateQ7/:id", (req, res) => {
+    const id = ObjectID(req.params.id);
+    const q7 = req.body;
+    console.log(id, q7);
+    leadsCollection
+      .updateOne(
+        { _id: id },
+        {
+          $set: {
+            answer7: q7,
+          },
+        }
+      )
+      .then((result) => {
+        console.log(result);
+        res.send(result.modifiedCount > 0);
+      });
+  });
+  app.patch("/updateQ8/:id", (req, res) => {
+    const id = ObjectID(req.params.id);
+    const q8 = req.body;
+    console.log(id, q8);
+    leadsCollection
+      .updateOne(
+        { _id: id },
+        {
+          $set: {
+            answer8: q8,
+          },
+        }
+      )
+      .then((result) => {
+        console.log(result);
+        res.send(result.modifiedCount > 0);
+      });
+  });
   app.patch("/finalUpdate/:id", (req, res) => {
     const id = ObjectID(req.params.id);
     const update = req.body;
     console.log(id);
-    leadsCollection.updateOne(
-      { _id: id },
-      {
-        $set: {
-          answer1: update.answer1,
-          answer2: update.answer2,
-          answer3: update.answer3,
-          answer4: update.answer4,
-          answer4dot1: update.answer4dot1,
-          answer5: update.answer5,
-          answer5dot1: update.answer5dot1,
-          answer5dot2: update.answer5dot2,
-          answer5dot3: update.answer5dot3,
-          answer6: update.answer6,
-          answer7: update.answer7,
-          answer8: update.answer8,
-        },
-      }
-    )
-    .then((result) =>{
-      console.log(result);
-      res.send(result.modifiedCount>0)
-    })
+    leadsCollection
+      .updateOne(
+        { _id: id },
+        {
+          $set: {
+            answer1: update.answer1,
+            answer2: update.answer2,
+            answer3: update.answer3,
+            answer4: update.answer4,
+            answer4dot1: update.answer4dot1,
+            answer5: update.answer5,
+            answer5dot1: update.answer5dot1,
+            answer5dot2: update.answer5dot2,
+            answer5dot3: update.answer5dot3,
+            answer6: update.answer6
+          },
+        }
+      )
+      .then((result) => {
+        console.log(result);
+        res.send(result.modifiedCount > 0);
+      });
   });
 });
 
